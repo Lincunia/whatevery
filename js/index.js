a14b777ccf4aa747dbbf34d6f4703d07d4d9a890
Lincunia <pll52742@gmail.com>
2023-09-13 23:20:47 -0500

    After doing a test in my college

--- js/index.js
+++ js/index.js
@@ -1,31 +1,8 @@
-import katex from 'https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.mjs';
-document.addEventListener('DOMContentLoaded', ()=>{
-  const myMappings = [
-    ['a', 'y = x^2', '../images/pic1.png'],
-    ['b', 'y = x^2 +3', '../images/pic2.png'],
-    ['c', 'y = x^2 -3', '../images/pic3.png'],
-    ['d', 'y = \\left(x+3\\right)^2', '../images/pic4.png'],
-    ['e', 'y = \\left(x-3\\right)^2', '../images/pic5.png'],
-    ['f', 'y = 3x^2', '../images/pic6.png'],
-    ['g', 'y = -3x^2', '../images/pic7.png'],
-    ['h', 'y = \\frac{x^3}{3}', '../images/pic8.png'],
-    ['i', 'y = \\left(x-3\\right)^2 +2', '../images/pic9.png'],
-    ['j', 'y = -\\left(x-3\\right)^2 +2', '../images/pic10.png']
-  ];
-  const pics=document.getElementById('pictures');
-  for(const val of myMappings){
-    const newElmt=document.createElement('div');
-    newElmt.innerHTML=`<div class="uk-background-muted uk-padding uk-panel">
-              <p id="${val[0]}" style="color: black"></p>
-              <div
-                class="uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light"
-                data-src="${val[2]}"
-                uk-img
-              ></div>
-            </div>`;
-    pics.appendChild(newElmt)
-    katex.render(val[1], document.getElementById(val[0]), {
-      throwOnError: false,
-    });
-  }
-});
+import katex from "https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.mjs";
+
+export function renderFormula(arg, id) {
+  katex.render(arg, document.getElementById(id), {
+    throwOnError: false,
+  });
+}
+
