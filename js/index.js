4a3d427a2eb08749e525668b6dd7415a237fc6bc
Lincunia <pll52742@gmail.com>
2023-09-27 21:20:33 -0500

    Just a little addition to JS

--- js/index.js
+++ js/index.js
@@ -1,8 +1,34 @@
 import katex from "https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.mjs";
 
-export function renderFormula(arg, id) {
+function toRender(arg, id) {
   katex.render(arg, document.getElementById(id), {
     throwOnError: false,
   });
 }
+
+function KaTeXRenderWithAttributes(elmtParent, elmtChild, arg, id) {
+  elmtChild.setAttribute("id", id);
+  elmtParent.appendChild(elmtChild);
+  elmtParent.appendChild(document.createElement("br"));
+  toRender(arg, id);
+}
+function RenderInnerHTML(elmtParent, elmtChild, innerContent) {
+  elmtChild.innerHTML = innerContent;
+  elmtParent.appendChild(elmtChild);
+}
+function KaTeXRenderWithInnerHTML(
+  elmtParent,
+  elmtChild,
+  innerContent,
+  arg,
+  id,
+) {
+  RenderInnerHTML(elmtParent, elmtChild, innerContent);
+  toRender(arg, id);
+}
+export {
+  KaTeXRenderWithAttributes,
+  RenderInnerHTML,
+  KaTeXRenderWithInnerHTML,
+};
 
